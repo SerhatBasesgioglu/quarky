44c5200063a779faea5b382af72495e8c11b0cb9
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2025-08-22 17:44:33 +0300

    feat: add crud note controller, create dbcontext, note model

---
+++ backend/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quarky;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options) { }
+    public DbSet<Note> Notes => Set<Note>();
+}
