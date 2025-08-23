f15d74a480091d200438e2d5c811a3ab142a8735
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2025-08-23 21:32:34 +0300

    feat: add basic ui notes, add tailwind and primeng

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-import { ConceptsComponent } from './concepts/concepts.component';
-import { ConceptEditComponent } from './concept-edit/concept-edit.component';
-import { RoadmapComponent } from './roadmap/roadmap.component';
+import { NotesComponent } from './features/notes/notes.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'concepts' },
-  { path: 'concepts', component: ConceptsComponent },
-  { path: 'concepts/new', component: ConceptsComponent },
-  { path: 'concepts/:id', component: ConceptEditComponent },
-  { path: 'roadmap', component: RoadmapComponent },
-  { path: '**', redirectTo: 'concepts' },
+  { path: '**', redirectTo: 'notes', pathMatch: 'full' },
+  { path: 'notes', component: NotesComponent },
 ];
